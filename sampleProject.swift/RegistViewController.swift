f608dce754887157435fd1dc019fe9376ca3b9b3
榎本旭晃 <kingdomhearts.sora319@gmail.com>
2019-07-04 15:48:33 +0800

    機能変更・レイアウト変更
    設定画面にTableView,TableViewCellを追加して、cellをタップしたら画面遷移する
    初期ユーザー登録時にRealmを使用してデータ保存
    UserクラスをUser.swiftにて作成
    カレンダー画面のレイアウト変更

--- sampleProject.swift/RegistViewController.swift
+++ sampleProject.swift/RegistViewController.swift
@@ -9,21 +9,25 @@
 import UIKit
 import RealmSwift
 
-
 class RegistViewController: UIViewController {
+    
+//    対象となるuserをnilで定義
+    var user: User? = nil
+    
     @IBOutlet weak var userNameTextField: UITextField!
     @IBOutlet weak var userLabel: UILabel!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
     @IBAction func didClickBtn(_ sender: Any) {
         let userName = userNameTextField.text
 //        テキストフィールドの値が空じゃない場合は遷移する
         if userNameTextField.text != "" {
+            
+            registUser(with: userName!)
             performSegue(withIdentifier: "toStart", sender: userName)
         } else { // 値が空の場合はアラートを表示する
             let alert = UIAlertController(title: "未入力", message: "ユーザー名が入力されていません", preferredStyle: .alert)
@@ -35,8 +39,37 @@ class RegistViewController: UIViewController {
             alert.addAction(yesAction)
             //        アラートを表示する
             present(alert, animated: true, completion: nil)
+            
         }
     }
     
 }
 
+//  Realmに関する処理
+extension RegistViewController {
+//    UserクラスはUser.swiftで定義
+//    User作成
+    func registUser(with name:String) {
+//        Realmに接続
+        let realm = try! Realm()
+        print(Realm.Configuration.defaultConfiguration.fileURL!)
+        
+//        登録処理
+//        USERの初期化
+        user = User()
+        user?.id = getMaxId()
+        user?.name = name
+        user?.date = Date()
+        
+//        Realmに書き込み
+        try! realm.write{   // Realmの書き込みモード
+            realm.add(user!)    // DBに追加
+        }
+    }
+    
+//    getMaxId
+    func getMaxId() -> Int {
+        let realm = try! Realm()
+        return (realm.objects(User.self).max(ofProperty: "id") as Int? ?? 0) + 1
+    }
+}
--- sampleProject.swift/User.swift
+++ sampleProject.swift/User.swift
@@ -6,4 +6,14 @@
 //  Copyright © 2019 TeruakiEnomoto. All rights reserved.
 //
 
-import Foundation
+import RealmSwift
+
+// Realmではテー出る定義をオブジェクト定義を行う
+// 必ずObjectクラスを継承すること
+class User: Object {
+//    カラムの定義　兼　メンバ変数の定義
+    @objc dynamic var id: Int = Int()
+    @objc dynamic var name:String = String()
+    @objc dynamic var date:Date = Date()
+    
+}
