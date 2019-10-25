1aecf3f44f39d5de1006792733910b3bbed019b3
榎本旭晃 <kingdomhearts.sora319@gmail.com>
2019-10-25 22:50:09 +0900

    Share Extension機能追加
    記録したポジティブとネガティブの内容（テキストと写真）を他のアプリケーションでも共有できるようにするため。
    Share Extension用のファイル作成と共有するものを設定。次は共有するときの動作を処理するためのコードを書く。

---
+++ testShareExtension/ShareViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ShareViewController.swift
+//  testShareExtension
+//
+//  Created by TeruakiEnomoto on 2019/10/25.
+//  Copyright © 2019 TeruakiEnomoto. All rights reserved.
+//
+
+import UIKit
+import Social
+
+class ShareViewController: SLComposeServiceViewController {
+
+//    文字が入力されていないとPostを無効にする
+    override func isContentValid() -> Bool {
+        // Do validation of contentText and/or NSExtensionContext attachments here
+        return true
+    }
+
+//    Postを押したあとの処理
+    override func didSelectPost() {
+        // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
+    
+        // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
+        self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+    }
+
+//    追加項目のリスト管理
+    override func configurationItems() -> [Any]! {
+        // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
+        return []
+    }
+
+}
